4a47ebd94dcb39e21f2baac88991d57d9a334713
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-06 20:40:00 -0300

    Create exercicio_3.sql

---
+++ bdii_ifpr_2023/trimestre_1/exercicio_3.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE universidades;
+USE universidades;
+
+CREATE TABLE alunos (
+	id_aluno INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_aluno VARCHAR(50) NOT NULL,
+    endereco_aluno VARCHAR(255) NOT NULL,
+    cidade VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE disciplinas (
+	id_disciplina INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    carga_horaria INT NOT NULL
+);
+
+CREATE TABLE professores (
+	id_professor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_professor VARCHAR(50) NOT NULL,
+	endereco_professor VARCHAR(255) NOT NULL,
+    cidade VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE turma (
+	id_turma INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	disciplina_id INT NOT NULL REFERENCES disciplinas (id_disciplina),
+    professor_id INT NOT NULL REFERENCES professores (id_professor),
+    horario TIME NOT NULL
+);
+
+CREATE TABLE historico (
+	aluno_id INT NOT NULL REFERENCES alunos (id_aluno),
+    disciplina_id INT NOT NULL REFERENCES disciplinas (id_disciplina),
+    professor_id INT NOT NULL REFERENCES professores (id_professor),
+    frequencia INT NOT NULL,
+    nota DECIMAL(2, 2) NOT NULL
+);
